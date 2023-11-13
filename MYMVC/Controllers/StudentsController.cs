b790191f409cbeab51040d8b84e917255a05dfb0
andremugabo <andremugabo@yahoo.fr>
2023-11-13 21:11:35 +0200

    second commit

---
+++ MYMVC/Controllers/StudentsController.cs
@@ -0,0 +1,24 @@
+﻿using MYMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MYMVC.Controllers
+{
+    public class StudentsController : Controller
+    {
+        // GET: Students
+        public ActionResult Index()
+        {
+            List<Students> allStudents = new List<Students>();
+            allStudents.Add(new Students(20000, "Munezero Ange", "Female",90));
+            allStudents.Add(new Students(20001, "Munezero Ange1", "Female1",904));
+            allStudents.Add(new Students(20002, "Munezero Ange2", "Female2",904));
+            allStudents.Add(new Students(20003, "Munezero Ange3", "Female3",904));
+            allStudents.Add(new Students(20004, "Munezero Ange4", "Female4",905));
+            return View("Index",allStudents);
+        }
+    }
+}
---
+++ MYMVC/Models/Students.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MYMVC.Models
+{
+    public class Students
+    {
+        public int id { get; set; }
+        public String studentName { get; set; }
+        public String gender { get; set;}
+        public float marks { get; set; }
+
+        public Students()
+        {
+            id = -1;
+            studentName = "No Name";
+            gender = "Unspesfied";
+            marks = 0;
+        }
+
+        public Students(int id,String studentName,String gender,float marks)
+        {
+            this.id = id;
+            this.studentName = studentName;
+            this.gender = gender;
+            this.marks = marks;
+
+        }
+    }
+}
